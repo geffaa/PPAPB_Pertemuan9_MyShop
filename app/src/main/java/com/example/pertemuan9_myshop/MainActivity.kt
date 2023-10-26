4e8ad9a84d15b1691475f53aaac4fd9ded77de5e
geffaa <yodhimas02@gmail.com>
2023-10-26 09:19:53 +0700

    add bottom navigation

--- app/src/main/java/com/example/pertemuan9_myshop/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan9_myshop/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.pertemuan9_myshop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.pertemuan9_myshop.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding){
+            val navvController = findNavController(R.id.nav_host_fragment)
+            bottomNavigationView.setupWithNavController(navvController)
+        }
     }
 }
